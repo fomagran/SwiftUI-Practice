cf7476035002237fc858868c03fb544db96221ac
Fomagran <fomagran6@naver.com>
2021-11-07 15:45:26 +0900

    Initial Commit

---
+++ SwiftUI Practice/SwiftUI_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PracticeApp.swift
+//  SwiftUI Practice
+//
+//  Created by Fomagran on 2021/11/07.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
