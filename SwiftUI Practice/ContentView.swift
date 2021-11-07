1b8e17fab713dc0de17ea75f9d86b3537d9272e3
Fomagran <fomagran6@naver.com>
2021-11-07 16:22:42 +0900

    [add] @State 사용해보기

--- SwiftUI Practice/ContentView.swift
+++ SwiftUI Practice/ContentView.swift
@@ -8,14 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isEnabled:Bool = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .center, spacing:60) {
+            Text(isEnabled ? "가능" : "불가능")
+            
+            Button {
+                isEnabled.toggle()
+            } label: {
+                Text(isEnabled ? "불가능":"가능")
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .previewDevice("iPhone 13 Pro")
+        }
     }
 }
